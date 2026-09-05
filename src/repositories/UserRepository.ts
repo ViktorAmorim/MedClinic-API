d4e88278e66525a03a650898b0b196794aa1a51a
ViktorAmorim <joao.victor.amorim.martins@hotmail.com>
2026-09-05 01:36:17 -0300

    feat: Implementação do UserRepository

--- src/repositories/UserRepository.ts
+++ src/repositories/UserRepository.ts
@@ -1,1 +1,4 @@
-//UserRepository.ts
+import { AppDataSource } from "../database/data-source";
+import { User } from "../entities/User";
+
+export const UserRepository = AppDataSource.getRepository(User);
