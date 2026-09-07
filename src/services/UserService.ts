e4c4a77c2a4e3812515ee759dcbf48254b16f9f2
ViktorAmorim <joao.victor.amorim.martins@hotmail.com>
2026-09-07 17:16:40 -0300

    feat (auth): implementar serviço de criação de usuario

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,1 +1,26 @@
-//UserService
+import { User } from "../entities/User";
+import { UserRepository } from "../repositories/UserRepository";
+import { CreateUserDTO } from "../dtos/CreateUserDTO";
+import { AppError } from "../errors/AppError";
+import { hashPassword } from "../utils/password";
+
+export class UserService {
+  async createUser(userData: CreateUserDTO): Promise<User> {
+    const existingUser = await UserRepository.findOne({
+      where: { email: userData.email },
+    });
+
+    if (existingUser) {
+      throw new AppError("Email já cadastrado.", 409);
+    }
+
+    const hashedPassword = await hashPassword(userData.senha);
+
+    const newUser = UserRepository.create({
+      ...userData,
+      senha: hashedPassword,
+    });
+
+    return await UserRepository.save(newUser);
+  }
+}
