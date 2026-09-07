75ee6cc510e56370464c4f1792845e76952005a3
ViktorAmorim <joao.victor.amorim.martins@hotmail.com>
2026-09-07 17:17:35 -0300

    feat (auth): atualizar a entidade usuario no campo email para unique

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -18,7 +18,7 @@ export class User {
   @Column("varchar", { length: 255 })
   nome!: string;
 
-  @Column("varchar", { length: 255 })
+  @Column("varchar", { length: 255, unique: true })
   email!: string;
 
   @Column("varchar")
