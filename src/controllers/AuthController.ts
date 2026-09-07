ac50640fc84cc61a869c7b1ddad840b3ffec40f9
ViktorAmorim <joao.victor.amorim.martins@hotmail.com>
2026-09-07 17:17:00 -0300

    feat (auth): aimplementar controller de cadastro

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,1 +1,23 @@
-//AuthController.ts
+import { Request, Response } from "express";
+import { UserService } from "../services/UserService";
+
+export class AuthController {
+  async register(req: Request, res: Response): Promise<Response> {
+    const { nome, email, senha } = req.body;
+
+    if (!nome || !email || !senha) {
+      return res
+        .status(400)
+        .json({ message: "Nome, email e senha são obrigatórios" });
+    }
+
+    const userService = new UserService();
+    const user = await userService.createUser({ nome, email, senha });
+
+    const { senha: _, ...userWithoutPassword } = user;
+
+    return res.status(201).json(userWithoutPassword);
+  }
+
+  //async login(req: Request, res: Response): Promise<Response> {}
+}
