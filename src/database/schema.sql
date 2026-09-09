bb6bf9b2dcc933647b76b80506c315a7d83926a4
ViktorAmorim <joao.victor.amorim.martins@hotmail.com>
2026-09-09 15:43:20 -0300

    chore: create users table in database schema

---
+++ src/database/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    nome VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    senha VARCHAR NOT NULL,
+    role user_role NOT NULL DEFAULT 'ATENDENTE',
+    criado_em TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
